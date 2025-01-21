b6d5a4bddea2e812df060ab8b68427a77fcbdffd
Raghavendra J <Raghavendrajraaghu33@gmail.com>
2025-01-21 23:07:40 +0530

    After changing the proper client information

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,74 +1,128 @@
-import React from 'react'
-import { Card } from 'react-bootstrap';
-import {FaCheckCircle, FaMoneyBillWave, FaPaintRoller, FaFileAlt, FaHardHat, FaBuilding } from 'react-icons/fa';
+import React from "react";
+import { Card } from "react-bootstrap";
+import {
+  FaCheckCircle,
+  FaMoneyBillWave,
+  FaPaintRoller,
+  FaFileAlt,
+  FaHardHat,
+  FaBuilding,
+} from "react-icons/fa";
 
-export default function Footer()
- {
+export default function Footer() {
   return (
     <div>
-      <h1 className="text-center" style={{fontFamily:"Roboto, serif"}}>Why Choose Nilaya </h1>
-<h1  className="text-center" style={{color:'orange',fontFamily:"Roboto, serif"}}>Builders</h1><br></br><br></br><br></br>
+      <h1 className="text-center" style={{ fontFamily: "Roboto, serif" }}>
+        Why Choose Nilaya{" "}
+      </h1>
+      <h1
+        className="text-center"
+        style={{ color: "orange", fontFamily: "Roboto, serif" }}
+      >
+        Builders
+      </h1>
+      <br></br>
+      <br></br>
+      <br></br>
 
-<div className='flex icon1' style={{fontFamily:"Roboto, serif"}}>
-<div className='icon-box' style={{marginLeft:"100px"
-}}>
-  <FaCheckCircle  size={30} color="black" /><br></br>
-  <h5>Quality truly delivered</h5>
-  </div>
-  <div className='icon-box' style={{marginLeft:"300px"}}>
-  <FaMoneyBillWave    size={30} color="black" /><br></br>
-  <h5>Legit payment process</h5>
-  </div>
-  <div className='icon-box' style={{marginLeft:"300px"}}>
-  <FaPaintRoller   size={30} color="black" /><br></br>
-  <h5>Post-sale services</h5>
-  </div> 
-</div><br></br><br></br><br></br><br></br>
+      <div className="flex icon1" style={{ fontFamily: "Roboto, serif" }}>
+        <div className="icon-box" style={{ marginLeft: "100px" }}>
+          <FaCheckCircle size={30} color="black" />
+          <br></br>
+          <h5>Quality truly delivered</h5>
+        </div>
+        <div className="icon-box" style={{ marginLeft: "300px" }}>
+          <FaMoneyBillWave size={30} color="black" />
+          <br></br>
+          <h5>Legit payment process</h5>
+        </div>
+        <div className="icon-box" style={{ marginLeft: "300px" }}>
+          <FaPaintRoller size={30} color="black" />
+          <br></br>
+          <h5>Post-sale services</h5>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
 
-<div className='flex icon2' style={{fontFamily:"Roboto, serif"}}>
-<div className='icon-box' style={{marginLeft:"80px"}}>
-  <FaBuilding  size={30} color="black" /><br></br>
-  <h5>Ethical i.e., harassment free</h5>
-  </div>
-  <div className='icon-box' style={{marginLeft:"290px"}}>
-  <FaFileAlt   size={30} color="black" /><br></br>
-  <h5>Clarity of work-flow</h5>
-  </div>
-  <div className='icon-box' style={{marginLeft:"290px"}}>
-  <FaHardHat   size={30} color="black" /><br></br>
-  <h5>Team of actual experts</h5>
-  </div> 
-</div><br></br><br></br><br></br><br></br>
+      <div className="flex icon2" style={{ fontFamily: "Roboto, serif" }}>
+        <div className="icon-box" style={{ marginLeft: "80px" }}>
+          <FaBuilding size={30} color="black" />
+          <br></br>
+          <h5>Ethical i.e., harassment free</h5>
+        </div>
+        <div className="icon-box" style={{ marginLeft: "290px" }}>
+          <FaFileAlt size={30} color="black" />
+          <br></br>
+          <h5>Clarity of work-flow</h5>
+        </div>
+        <div className="icon-box" style={{ marginLeft: "290px" }}>
+          <FaHardHat size={30} color="black" />
+          <br></br>
+          <h5>Team of actual experts</h5>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
 
+      <div>
+        <img
+          src="https://t4.ftcdn.net/jpg/09/23/12/47/360_F_923124779_maah7DQGmzRUwkY1Ma4VUmZghxpf6G8J.jpg"
+          width="100%"
+        />
 
-<div>
-  <img  src="https://t4.ftcdn.net/jpg/09/23/12/47/360_F_923124779_maah7DQGmzRUwkY1Ma4VUmZghxpf6G8J.jpg" width="100%"/>
+        <Card
+          className="footercard"
+          style={{ background: "black", color: "white", height: "760px" }}
+        >
+          <Card.Body>
+            <Card.Title>
+              <h2 style={{ textAlign: "center" }}>Nilaya Builders</h2>
+            </Card.Title>
+            <br></br>
+            <Card.Text>
+              <p style={{ marginLeft: "300px" }}>
+                USEFUL LINKS
+                <ul style={{ listStyleType: "disc", marginLeft: "2px" }}>
+                  <li>
+                    <a className="footerlinks" href="/Aboutus">
+                      About us
+                    </a>
+                  </li>
+                  <li>
+                    <a className="footerlinks" href="/Careers">
+                      Careers
+                    </a>
+                  </li>
+                  <li>
+                    <a className="footerlinks" href="/Contact">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </p>
+            </Card.Text>
+            <Card.Text style={{ marginLeft: "900px", marginTop: "-120px" }}>
+              <p>
+                Address : #13/1, 14th Main Rd, <br></br>2nd Cross, <br></br>
+                Saraswathi puram, Mysuru - 570009, <br></br>Karnataka<br></br>
+                Phone : 8197339552<br></br>
+                Email : anilayaconstructions@gmail.com
+              </p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <br></br>
 
-
-    <Card className='footercard'  style={{background:"black",color:"white",height:"760px"}}>
-      <Card.Body>
-        <Card.Title><h2 style={{textAlign:"center"}}>Nilaya Builders</h2></Card.Title><br></br>
-        <Card.Text>
-        <p style={{marginLeft:"300px"}}>USEFUL LINKS
-        <ul  style={{ listStyleType: "disc", marginLeft: "2px" }}>
-          <li><a className='footerlinks' href="/Aboutus">About us</a></li>
-          <li><a className='footerlinks' href="/Careers" >Careers</a></li>
-          <li><a className='footerlinks' href="/Contact" >Contact Us</a></li>
-          </ul>
-          </p>
-        </Card.Text>
-        <Card.Text style={{marginLeft:"900px",marginTop:"-120px"}}>
-        <p>Address : #xxx, 6th Main Rd, <br></br>Vijay Nagar 2nd Stage, <br></br>Vijayanagar, Mysuru, <br></br>Karnataka 570017<br></br>
-        Phone : 096069 66266<br></br>
-        Email : eo@nilaya.in</p>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-
-</div><br></br>
-
-
-<p style={{textAlign:"center"}}>Nilaya Builders – Copyright 2022. Developed by Qurativity Media Solutions Private Limited</p>
+      <p style={{ textAlign: "center" }}>
+        Ailayaconstructions – Copyright 2025. Developed by Raj IT Solutions
+        Private Limited
+      </p>
     </div>
-  )
+  );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,71 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Navbar as BootstrapNavbar } from 'react-bootstrap'; // Renaming the import to avoid conflict
-import { Link } from 'react-router-dom';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Navbar as BootstrapNavbar } from "react-bootstrap"; // Renaming the import to avoid conflict
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <BootstrapNavbar expand="lg" className="bg-body-tertiary bg-slate-500 shadow-lg h-20">
+    <BootstrapNavbar
+      expand="lg"
+      className="bg-body-tertiary bg-slate-500 shadow-lg h-20"
+    >
       <Container>
         {/* Applying grid classes to the navbar */}
         <div className="grid grid-cols-12 w-full items-center">
           {/* Brand - Takes up 3 columns */}
-          <BootstrapNavbar.Brand className="col-span-3"  style={{ fontFamily: "Roboto, serif" }}>
-            Nilaya Constructors
+          <BootstrapNavbar.Brand
+            className="col-span-3"
+            style={{ fontFamily: "Roboto, serif" }}
+          >
+            Nilaya Constuctions
           </BootstrapNavbar.Brand>
 
           {/* Navbar toggle (for mobile view) */}
-          <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" className="col-span-1" />
+          <BootstrapNavbar.Toggle
+            aria-controls="basic-navbar-nav"
+            className="col-span-1"
+          />
 
           {/* Navbar items - Use grid to apply custom layout */}
-          <BootstrapNavbar.Collapse id="basic-navbar-nav" className="col-span-8 ml-40 flex">
+          <BootstrapNavbar.Collapse
+            id="basic-navbar-nav"
+            className="col-span-8 ml-40 flex"
+          >
             <Nav className="me-auto grid grid-cols-4 gap-2 w-full navs">
-              <Nav.Link  as={Link}   to="/" className="text-center">HOME</Nav.Link>
-              <NavDropdown title="CATEGORY" id="basic-nav-dropdown " className="text-center">
-                <NavDropdown.Item as={Link}  to="/Residential">Residential</NavDropdown.Item>
+              <Nav.Link as={Link} to="/" className="text-center">
+                HOME
+              </Nav.Link>
+              <NavDropdown
+                title="CATEGORY"
+                id="basic-nav-dropdown "
+                className="text-center"
+              >
+                <NavDropdown.Item as={Link} to="/Residential">
+                  Residential
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link}  to="Commercial">Commercial</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="Commercial">
+                  Commercial
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link}  to="Interiors">Interiors</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="Interiors">
+                  Interiors
+                </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link  as={Link}   to="/Aboutus" className="text-center">ABOUT</Nav.Link>
-              <Nav.Link  as={Link}   to="/Careers" className="text-center">CAREERS</Nav.Link>
-              <Nav.Link as={Link}   to="/Projects" className="text-center">PROJECTS</Nav.Link>
-              <Nav.Link as={Link}   to="/Contact" className="text-center">CONTACT</Nav.Link>
+              <Nav.Link as={Link} to="/Aboutus" className="text-center">
+                ABOUT
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Careers" className="text-center">
+                CAREERS
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Projects" className="text-center">
+                PROJECTS
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Contact" className="text-center">
+                CONTACT
+              </Nav.Link>
             </Nav>
           </BootstrapNavbar.Collapse>
         </div>
